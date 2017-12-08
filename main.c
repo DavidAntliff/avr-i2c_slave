3f6c4b3e1f86a5a239f643aafc9447a20db8afbd
David Antliff <david@pitchblende.co.nz>
2017-12-08 22:37:21 +1300

    Tidy up main.

--- main.c
+++ main.c
@@ -32,68 +32,66 @@
 
 #include "usitwislave/usitwislave.h"
 
+#define I2C_ADDRESS 0x44
+
+#define WRITE_LED PA0
+#define READ_LED  PA1
+
+// single-byte register storage
 static uint8_t data = 0;
 
 static void data_callback(uint8_t input_buffer_length, const uint8_t *input_buffer,
-                   uint8_t *output_buffer_length, uint8_t *output_buffer);
+                          uint8_t *output_buffer_length, uint8_t *output_buffer);
+
+static void idle_callback(void);
+
+
+static void blink_led(volatile uint8_t * port, uint8_t pin, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        *port |= 1 << pin;
+        _delay_ms(150);
+        *port &= ~(1 << pin);
+        _delay_ms(150);
+    }
+}
 
 static void data_callback(uint8_t input_buffer_length, const uint8_t *input_buffer,
                    uint8_t *output_buffer_length, uint8_t *output_buffer)
 {
+    // This is a simple handler for a single I2C register interface.
+    // The first byte of a write is stored, and retrieved on a read.
+
     if (input_buffer_length > 0)
     {
         // write
         data = input_buffer[0];
-        for (int i = 0; i < data; ++i)
-        {
-			PORTA ^= 0b00000001;
-            _delay_ms(150);
-			PORTA ^= 0b00000001;
-		    _delay_ms(150);
-        }
+        blink_led(&PORTA, WRITE_LED, data);
     }
     else
     {
    	    // read
-	    PORTA ^= 0b00000010;
-	    _delay_ms(150);
-	    PORTA ^= 0b00000010;
-	    _delay_ms(150);
+        blink_led(&PORTA, READ_LED, data);
     }
 
+    // always populate the output buffer, even for a read, otherwise a subsequent read will return an empty buffer.
     *output_buffer_length = 1;
     output_buffer[0] = data;
 }
 
-static void idle_callback(void);
-
 static void idle_callback(void)
 {
-    /* _delay_ms(950); */
-    /* PORTA = ~PORTA; */
-    /* _delay_ms(50); */
-    /* PORTA = ~PORTA; */
+    //blink_led(&PORTA, READ_LED, 1);
 }
 
 int main(void)
 {
-    DDRA = 0x01;                       // initialize port A
+    // initialize port A
+    DDRA = (1 << PA0) | (1 << PA1);
     PORTA = 0b00000000;
 
-    /* while(1) */
-    /* { */
-    /*     // LED on */
-    /*     PORTA = 0b00000001;            // PA0 = High = Vcc */
-    /*     _delay_ms(500);                // wait 500 milliseconds */
-
-    /*     //LED off */
-    /*     PORTA = 0b00000000;            // PA0 = Low = 0v */
-    /*     _delay_ms(500);                // wait 500 milliseconds */
-    /* } */
-
-    uint8_t slave_address = 0x44;
-    bool use_sleep = false;
-
-    usi_twi_slave(slave_address, use_sleep, data_callback, idle_callback);
+    // start the slave loop
+    usi_twi_slave(I2C_ADDRESS, false /*use_sleep*/, data_callback, idle_callback);
 }
 
